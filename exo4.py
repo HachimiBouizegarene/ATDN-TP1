400822e7f38f4dbf7d447f4e45d27a568c63bb11
Hachimi Bouizegarene <hachimi.bouizegarene@outlook.fr>
2025-03-27 23:07:04 +0100

    exo 4

--- exo4.py
+++ exo4.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.decomposition import KernelPCA
+from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("donnees_elevage_poulet.csv")
 data_normalized = (data - data.mean()) / data.std()
@@ -20,4 +22,18 @@ plt.ylabel("2 composante principale")
 
 explained_variance_ratio = valeurs_propres_sorted / np.sum(valeurs_propres_sorted)
 print(f"Proportion de la variance expliquée par chaque composante principale: {explained_variance_ratio}")
+
+
+kernels = ["linear", "rbf", "poly"]
+plt.figure(figsize=(15, 5))
+for i, kernel in enumerate(kernels):
+    kpca = KernelPCA(kernel=kernel)
+    data_kpca = kpca.fit_transform(data_normalized)
+    variance_expliquee = kpca.eigenvalues_ / np.sum(kpca.eigenvalues_)  
+    plt.subplot(1, 3, i + 1)
+    plt.scatter(data_kpca[:, 0], data_kpca[:, 1])
+    plt.title(f"KernelPCA avec {kernel}\nVariance expliquée: {(variance_expliquee[0] + variance_expliquee[1]) * 100}")
+    plt.xlabel("1 composante principale")
+    plt.ylabel("2 composante principale")
 plt.show()
+
